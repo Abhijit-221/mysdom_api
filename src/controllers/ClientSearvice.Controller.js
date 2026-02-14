2a940c8e569caae4d61f8cf2ae05741f0761a432
abhijit <seain.abhi2021@gmail.com>
2026-02-15 00:38:50 +0530

    main: create API for clientService update and get

--- src/controllers/ClientSearvice.Controller.js
+++ src/controllers/ClientSearvice.Controller.js
@@ -2,7 +2,8 @@ const { validationResult } = require('express-validator');
 const { ResponseCodes } = require('../utils/constant');
 const ClientService = require('../models/ClienServiceSchema');
 const UserSchema = require('../models/UserSchema');
-
+const ClientSchema = require('../models/ClientSchema');
+const ServiceSchema = require('../models/ServiceSchema');
 module.exports = {
     /*
    * @route PIST /api/v1/mysdom/client-service/add
@@ -89,9 +90,13 @@ module.exports = {
             let { page, limit, search } = req.query;
             page = page || 1;
             limit = limit || 10;
+            console.log('page', page, limit);
             let skip = (page - 1) * limit;
             search = search?.trim();
-
+            let query = { is_deleted: false };
+            if(!['admin','superadmin'].includes(req.user.role)){
+                    query.clientId=req.user._id
+            }
             const pipeline = [
                 { $match: query },
 
@@ -100,7 +105,7 @@ module.exports = {
                         from: 'clients',
                         localField: 'clientId',
                         foreignField: '_id',
-                        as: 'clientDetails'
+                        as: 'clientDetails',
                     }
                 },
                 { $unwind: '$clientDetails' },
@@ -127,21 +132,28 @@ module.exports = {
                     }
                 });
             }
+            let countQiery = [...pipeline];
+            countQiery.push({
+                $group: {
+                    _id: null,
+                    count: { $sum: 1 }
+                },
+            });
 
             // pagination MUST come after search
             pipeline.push(
-                { $skip: skip },
-                { $limit: limit }
+                { $skip: parseInt(skip) },
+                { $limit: parseInt(limit) }
             );
 
             const clientServices = await ClientService.aggregate(pipeline);
-
-            const count = await ClientService.countDocuments(query);
+            // console.log('queryCOunt:', countQiery);
+            const clentServiceCount = await ClientService.aggregate(countQiery);
             return res.status(ResponseCodes.SUCCESS).json({
                 status: ResponseCodes.SUCCESS,
                 data: {
                     clientServices,
-                    count,
+                    count: clentServiceCount[0]?.count,
                 },
                 message: 'Client Service list fetched successfully'
             });
@@ -156,5 +168,129 @@ module.exports = {
             });
         }
     },
+    /*
+    * @route PIST /api/v1/mysdom/client-service/update
+    * @desc update a service within client
+    * @authentication  true [admin,user(which have specific client id) ]
+    */
+    updateClientService: async (req, res) => {
+        console.log('update Client Service API.....');
+        try {
+            const validationErrors = validationResult(req);
+            if (!validationErrors.isEmpty()) {
+                return res.status(ResponseCodes.BAD_REQUEST).json({
+                    status: ResponseCodes.BAD_REQUEST,
+                    data: {},
+                    errors: validationErrors.array(),
+                    message: 'Validation failed'
+                });
+            }
+            const {id, clientId, serviceId, tatDays,isActive } = req.body;
+
+            //let check the clienservice exist or not 
+            let checkClientSevice = await ClientService.findOne({
+                _id:id,
+                is_deleted:false
+            });
+            if(!checkClientSevice){
+                return res.status(ResponseCodes.NOT_FOUND).json({
+                    status:ResponseCodes.NOT_FOUND,
+                    data:{},
+                    error:"Invalid id",
+                    message:"Invalid id, client service not found",
+                })
+            }
+            //let check if role user with same clientId then it should allow
+            if (req.user.role === 'user') {
+                let getUser = await UserSchema.findOne({
+                    _id: req.user._id,
+                    clientId: req.body.clientId,
+                    is_deleted: false,
+                    isActive: true
+                });
+                if (!getUser) {
+                    return res.status(ResponseCodes.UNAUTHORIZED).json({
+                        status: ResponseCodes.UNAUTHORIZED,
+                        data: {},
+                        error: 'Unauthorized to assign service to this client',
+                        message: 'Unauthorized to assign service to this client'
+                    });
+                }
+            }
+            if(clientId){
+                //let check client exist or not 
+                let checkClient = await ClientSchema.findOne({
+                    _id:clientId,
+                    is_deleted:false
+                });
+                if(!checkClient){
+                    return res.status(ResponseCodes.NOT_FOUND).json({
+                        status:ResponseCodes.NOT_FOUND,
+                        data:{},
+                        error:"Invalid clientId",
+                        message:"Invalid clientId"
+                    })
+                }
+            }
+            if(serviceId){
+                let checkServices = await ServiceSchema.findOne({
+                    _id:serviceId,
+                    is_deleted:false
+                });
+                if(!checkServices){
+                    return res.status(ResponseCodes.NOT_FOUND).json({
+                        status:ResponseCodes.NOT_FOUND,
+                        data:{},
+                        error:"Invalid servaiceId",
+                        message:"Invalid serviceId, Service not found"
+                    })
+                }
+            }
+            //let check if client with same service already exists
+            if(clientId||serviceId){
+                const existingClientService = await ClientService.findOne({
+                    clientId: clientId || checkClientSevice.clientId,
+                    serviceId: serviceId || checkClientSevice.serviceId,
+                    is_deleted:false
+                });
+                if (existingClientService) {
+                    return res.status(ResponseCodes.CONFLICT).json({
+                        status: ResponseCodes.CONFLICT,
+                        data: {},
+                        error: 'Service already assigned with the client',
+                        message: 'Service already assigned with the client'
+                    });
+                }
+            }
+            let updatedData ={
+                ...clientId && {clientId},
+                ...serviceId && {serviceId},
+                ...tatDays && {tatDays},
+                updatedBy:req.user._id,
+                ...isActive!=undefined && {isActive}
+            } ;
+            // if(req.user.role = 'admin' || req.user.role)
+            console.log(updatedData);
+            let updatedClientService=await ClientService.findByIdAndUpdate({
+                is_deletd:false,
+                _id:id
+            },updatedData);
+            return res.status(ResponseCodes.SUCCESS).json({
+                status:ResponseCodes.SUCCESS,
+                data:updatedClientService,
+                message:"Client Service updated successfully."
+            })
+            
+        }
+        catch (error) {
+            console.error('Error in update Client Service List:', error);
+            return res.status(ResponseCodes.INTERNAL_SERVER_ERROR).json({
+                status: ResponseCodes.INTERNAL_SERVER_ERROR,
+                data: {},
+                error: error.message,
+                message: 'Server error'
+            });
+        }
+    }
 
 }
--- src/routes/ClientService.routes.js
+++ src/routes/ClientService.routes.js
@@ -11,9 +11,17 @@ const addServiceValidation = [
     body('tatDays').trim().notEmpty().isNumeric().withMessage('TAT Days must be a number')
 ];
 
+const updateServiceValidation = [
+    body('id').trim().notEmpty().isMongoId().withMessage('Id is requireded'),
+    body('clientId').optional().trim().isString().withMessage('Invalid Client ID'),
+    body('serviceId').optional().trim().isString().withMessage('Invalid ID is required'),
+    body('tatDays').optional().trim().isNumeric().withMessage('TAT Days must be a number')
+];
+
 // Route to add a new service
 router.post('/add', auth, authorize('admin', 'superadmin', 'user'), addServiceValidation, ClientServiceController.addClientService);
 router.get('/list', auth, authorize('admin', 'superadmin', 'user'), ClientServiceController.getClientServiceList);
+router.put('/update', auth, authorize('admin', 'superadmin', 'user'), updateServiceValidation, ClientServiceController.updateClientService);
 
 
 module.exports = router;
